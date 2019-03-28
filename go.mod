ee22cc1a0725a5730cea8652754bb05e08fe7cf6
Satoshi Tanaka <satoshi.tanaka@crowdworks.co.jp>
2019-03-28 11:59:22 +0900

    implement EC2 RI information(all)

--- go.mod
+++ go.mod
@@ -1,1 +1,6 @@
 module github.com/kangaechu/richecker
+
+require (
+	github.com/aws/aws-sdk-go v1.19.4
+	github.com/jessevdk/go-flags v1.4.0 // indirect
+)
--- richecker.go
+++ richecker.go
@@ -1,8 +1,50 @@
 package richecker
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"log"
+	"time"
+)
 
 func Check(accountId string, days int) {
 	fmt.Println("AccountID:", accountId)
 	fmt.Println("Days     :", days)
+
+	var timeout time.Duration
+	// All clients require a Session. The Session provides the client with
+	// shared configuration such as region, endpoint, and credentials. A
+	// Session should be shared where possible to take advantage of
+	// configuration and credential caching. See the session package for
+	// more information.
+	sess := session.Must(session.NewSession())
+
+	// Create a new instance of the service's client with a Session.
+	// Optional aws.Config values can also be provided as variadic arguments
+	// to the New function. This option allows you to provide service
+	// specific configuration.
+	svc := ec2.New(sess)
+
+	// Create a context with a timeout that will abort the upload if it takes
+	// more than the passed in timeout.
+	ctx := context.Background()
+	var cancelFn func()
+	if timeout > 0 {
+		ctx, cancelFn = context.WithTimeout(ctx, timeout)
+	}
+	// Ensure the context is canceled to prevent leaking.
+	// See context package for more information, https://golang.org/pkg/context/
+	if cancelFn != nil {
+		defer cancelFn()
+	}
+
+	dri := ec2.DescribeReservedInstancesInput{}
+	out, err := svc.DescribeReservedInstances(&dri)
+	if err != nil {
+		log.Fatalln("cannot get EC2 RI information.")
+	}
+	fmt.Println(out)
+
 }
