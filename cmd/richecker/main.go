45f221941ea5d552f322751aca78fb2bfcb30107
Satoshi Tanaka <satoshi.tanaka@crowdworks.co.jp>
2019-03-28 11:44:30 +0900

    parse command line options

--- cmd/richecker/main.go
+++ cmd/richecker/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/jessevdk/go-flags"
+	"github.com/kangaechu/richecker"
+	"os"
+)
+
+type CmdOpts struct {
+	AccountID string `short:"i" long:"accountid" description:"AWS Account ID" required:"true"`
+	Days      int    `short:"d" long:"days_before_expiration" description:"Print if expiration date is less than this days. (default:3)"`
+}
 
 func main() {
-	fmt.Println("hello!")
+	var opts CmdOpts
+	parser := flags.NewParser(&opts, flags.Default)
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		parser.WriteHelp(os.Stdout)
+		os.Exit(1)
+	}
+	richecker.Check(opts.AccountID, opts.Days)
 }
---
+++ richecker.go
@@ -0,0 +1,8 @@
+package richecker
+
+import "fmt"
+
+func Check(accountId string, days int) {
+	fmt.Println("AccountID:", accountId)
+	fmt.Println("Days     :", days)
+}
